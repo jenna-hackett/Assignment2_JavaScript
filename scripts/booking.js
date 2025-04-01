b5a4803c04700f545df224cca7ada5065aa2b81d
Jenna Hackett <jenna.hackett96@gmail.com>
2025-04-01 17:50:06 -0600

    updated booking.js
    start to JavaScript for the booking page.

--- scripts/booking.js
+++ scripts/booking.js
@@ -12,22 +12,52 @@ Version: April 1st, 2025
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
+// sets default cost and num of days selected
+let costPerDay = 35;
+let numOfDays = 0;
+let dayType = "full";
 
+// type of day buttons
+let fullDay = document.getElementById("full");
+let halfDay = document.getElementById("half");
+
+// clear button and total cost display
+let clearButton = document.getElementById("clear-button");
+let costDisplay = document.getElementById("calculated-cost");
+
+// selecting all day buttons... CAN I USE THIS????
+let dayElements = document.querySelectorAll(".day-selector li");
 
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
+// function to update the cost of booking calculator
+function updateCost() {
+    costDisplay.textContent = numOfDays * costPerDay;
+}
 
+// days clicked function
+function daysClicked(event) {
+    let clickedDay = event.target;
+    if (clickedDay.classList.contains("clicked")) {
+        clickedDay.classList.remove("clicked");
+        numOfDays--;
+    } else {
+        clickedDay.classList.add("clicked");
+        numOfDays++;
+    }
+    updateCost();
+}
 
-
+// adding event listeners to each day.
+dayElements.forEach(day => {
+    day.addEventListener("click", daysClicked);
+});
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
-
-
-
 
 
 
